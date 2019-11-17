9c875fabe5939138c9e5a9748251478dc8122ad7
Babatunde <babatundea15@gmail.com>
2019-11-17 06:58:57 +0100

    I installed project dependecies and layout structure of tool bar

---
+++ news-website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Toolbar from '../src/components/Toolbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Toolbar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ news-website/src/components/Toolbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Toolbar() {
+  return (
+    <header>
+      <nav>
+        <div></div>
+        <div>
+          <a href="#">THE LOGO</a>
+        </div>
+        <div>
+          <ul>
+            <a href="/">Latest News</a>
+            <a href="/">Users</a>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Toolbar;
