956ac66c3d46bc7ded14ecd0cd6fda44d01ca104
Babatunde <babatundea15@gmail.com>
2019-11-18 08:38:17 +0100

    I created the forms components and installed formik

--- news-website/src/App.js
+++ news-website/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import Toolbar from './components/Toolbar/Toolbar'
-import Sidedrawer from './components/Sidedrawer/Sidedrawer'
-import Backdrop from './components/Backdrop/Backdrop'
+import Register from '../src/components/Forms/Register'
+// import Toolbar from './components/Toolbar/Toolbar'
+// import Sidedrawer from './components/Sidedrawer/Sidedrawer'
+// import Backdrop from './components/Backdrop/Backdrop'
 
 
 function App() {
-  const [drawer,setDrawer] = React.useState(false)
-  const toggleButton = ()=>{setDrawer(!drawer)}
+  // const [drawer,setDrawer] = React.useState(false)
+  // const toggleButton = ()=>{setDrawer(!drawer)}
   return (
     <div style={{height:'100%'}}>
-      <Toolbar toggleButton={toggleButton}/>
+      <Register/>
+      {/* <Toolbar toggleButton={toggleButton}/>
       {drawer? <Backdrop toggleButton={toggleButton}/>: null}
       {drawer? <Sidedrawer toggleButton ={toggleButton}/> : null}
       <main style={{marginTop: '160px'}}>
       <p>This is the page content</p>
-      </main>
+      </main> */}
       </div>
   );
 }
---
+++ news-website/src/components/Forms/Register.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Register() {
+  return (
+    <form>
+      <label>
+        First Name
+        <input type="text" placeholder="First Name" name="firstname"></input>
+      </label>
+      <label>
+        Last Name
+        <input type="text" placeholder="Last Name" name="lastname"></input>
+      </label>
+      <label>
+        User Name
+        <input type="text" placeholder="User Name" name="username"></input>
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          placeholder="Choose a password"
+          name="password"
+        ></input>
+      </label>
+      <label>
+        Email
+        <input type="text" placeholder="Email" name="email"></input>
+      </label>
+      <label>
+        I have agreed to the terms and conditions
+        <input
+          type="checkbox"
+          name="terms"
+        ></input>
+      </label>
+      <input type="submit"></input>
+    </form>
+  );
+}
+
+export default Register;
