eb3b866ec2ce7c9edb6992b375bfee38cf82c34c
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-19 10:06:35 +0000

    Organised components

---
+++ hacker-news-app/src/Commonents/Backdrop/Backdrop.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Display = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 100;
+  top: 0;
+  left: 0;
+`;
+
+function Backdrop(props) {
+    
+  return <Display className="backdrop" onClick={()=>props.toggleButton()}></Display>;
+}
+
+export default Backdrop;
