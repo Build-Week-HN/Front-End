898e201186ad5487527e6f16d07fdded11dc85fa
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-21 22:45:50 +0000

    debug sidedrawer

--- src/Components/Sidedrawer/Sidedrawer.js
+++ src/Components/Sidedrawer/Sidedrawer.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const TopNav = styled.nav`
   height: 100%;
